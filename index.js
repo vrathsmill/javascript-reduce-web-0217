c27b8c44f095246a373384d555f21e8c9a924be3
vrathsmill <vlrathsmill@gmail.com>
2017-03-19 14:47:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+function getTotalAmountForProducts(products) {
+  let totalPrice = 0;
+
+  products.forEach(product => {
+    if (product.discount >= .5) {
+      totalPrice += product.price;
+    }
+  });
+
+  return totalPrice;
+}
+
+console.log(getTotalAmountForProducts(products)); // prints 25.5
+
+function getTotalAmountForProducts(products, callback) {
+  let totalPrice = 0;
+
+  products.forEach(product => {
+    totalPrice = callback(totalPrice, product);
+  });
+
+  return totalPrice;
+}
+
+// function callback(totalPrice, product) {
+//   if (product.discount >= .5) {
+//     return totalPrice + product.price;
+//   }
+//   return totalPrice;
+// }
+//
+// console.log(getTotalAmountForProducts(products, callback));
+
+function callback(totalPrice, product) {
+  if (product.price < 7) {
+    return totalPrice + product.price;
+  }
+  return totalPrice;
+}
+
+console.log(getTotalAmountForProducts(products, callback));
